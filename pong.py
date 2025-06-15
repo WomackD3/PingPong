08629e662b151d54854940c8127b7693b6990d53
Mack <D_wom3@yahoo.com>
2025-06-15 01:56:54 -0400

    handle collision of the paddle complete. working on the trajectory

--- pong.py
+++ pong.py
@@ -74,9 +74,13 @@ def handle_collision(ball, left_paddle, right_paddle):
         ball.y_vel *= -1
 
     if ball.x_vel <0:
-        if ball.y >= paddle.y and ball.y <= paddle.y + paddle.height:
-            if ball.x 
-
+        if ball.y >= left_paddle.y and ball.y <= left_paddle.y + left_paddle.height:
+            if ball.x - ball.radius <= left_paddle.x + left_paddle.width:
+                ball.x_vel *= -1
+    else:
+        if ball.y >= right_paddle.y and ball.y <= right_paddle.y + right_paddle.height:
+            if ball.x + ball.radius >= right_paddle.x:
+                ball.x_vel *= -1
 
 def handle_paddle_movement(keys, left_paddle, right_paddle):
     if keys[pygame.K_w] and left_paddle.y - left_paddle.VEL >= 0:
@@ -111,6 +115,7 @@ def main():
         handle_paddle_movement(keys, left_paddle, right_paddle)
 
         ball.move()
+        handle_collision(ball, left_paddle, right_paddle)
 
     pygame.quit()  
 
